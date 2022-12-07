69bb7aef3f50965065cd4e118938498839953290
Aman Kumar <Aman.Kumar17@in.ey.com>
2022-12-07 16:53:38 +0530

    del

--- ng-auth-services/src/main/java/com/ng/auth/exc_handler/UserNameFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.ng.auth.exc_handler;
-
-public class UserNameFoundException extends RuntimeException {
-
-    public UserNameFoundException(String message) {
-        super(message);
-    }
-}
